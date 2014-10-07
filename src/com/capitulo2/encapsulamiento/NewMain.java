642135b1a2ed48c7584ffaa977c8afffbff6b8b5
T <T@PC259>
2014-10-06 19:44:51 -0500

    Constructores

---
+++ src/com/capitulo2/encapsulamiento/NewMain.java
@@ -0,0 +1,15 @@
+
+package com.capitulo2.encapsulamiento;
+
+public class NewMain {
+
+   
+    public static void main(String[] args) {
+        Pato p1=new Pato();
+       // p1.setEdad(1);
+        p1.hacerCuac("cuac-cuac-cuac");
+        System.out.println(p1.getEdad());
+    }
+
+     
+}
---
+++ src/com/capitulo2/encapsulamiento/Vehiculo.java
@@ -0,0 +1,35 @@
+
+
+package com.capitulo2.encapsulamiento;
+
+
+public class Vehiculo {
+    private String Marca;
+    private int Year;
+    private float Precio;
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public int getYear() {
+        return Year;
+    }
+
+    public void setYear(int Year) {
+        this.Year = Year;
+    }
+
+    public float getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(float Precio) {
+        this.Precio = Precio;
+    }
+    
+    }
